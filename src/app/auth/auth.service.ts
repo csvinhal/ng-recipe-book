a22ae3db94e78b995c400bab258a1ca30e9515fd
csvinhal <cristiano.vinhal10@gmail.com>
2018-02-17 19:23:43 -0200

    sigin

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,9 +5,17 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(
-      error => {
-        console.log(error);
-      }
+      error => console.log(error)
     );
+  }
+
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        response => console.log(response)
+      )
+      .catch(
+        error => console.log(error)
+      );
   }
 }
