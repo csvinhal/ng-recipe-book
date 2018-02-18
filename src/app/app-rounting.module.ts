14a2ecee0b85b426a3da0999b8219d169bbf14ed
csvinhal <cristiano.vinhal10@gmail.com>
2018-02-17 23:41:05 -0200

    created modules

--- src/app/app-rounting.module.ts
+++ src/app/app-rounting.module.ts
@@ -1,12 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthGuard } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
@@ -16,41 +10,8 @@ const appRoutes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'recipes',
-    component: RecipesComponent,
-    children: [
-      {
-        path: '',
-        component: RecipeStartComponent
-      },
-      {
-        path: 'new',
-        component: RecipeEditComponent,
-        canActivate: [AuthGuard]
-      },
-      {
-        path: ':id',
-        component: RecipeDetailComponent,
-        canActivate: [AuthGuard]
-      },
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent,
-        canActivate: [AuthGuard]
-      }
-    ]
-  },
-  {
     path: 'shopping-list',
     component: ShoppingListComponent
-  },
-  {
-    path: 'signup',
-    component: SignupComponent
-  },
-  {
-    path: 'signin',
-    component: SigninComponent
   }
 ];
 
