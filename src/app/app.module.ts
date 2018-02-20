1c337d7cdfbaba400d33ed915c6b12a395aa6f02
csvinhal <cristiano.vinhal10@gmail.com>
2018-02-20 20:34:45 -0300

    starting with ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducers';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,8 @@ import { CoreModule } from './core/core.module';
     ShoppingListModule,
     AuthModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
   ],
   providers: [],
   bootstrap: [AppComponent]
